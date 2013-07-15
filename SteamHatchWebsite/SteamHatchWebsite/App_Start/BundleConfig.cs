69d2b854f4b7e50d126db1aec9f1f55c8b324dae
Anthony Kazyaka <ask.anthony@hotmail.com>
2013-07-15 17:58:36 -0400

    Making some basic bundles so that Bootstrap is included within the pages of the website.

--- SteamHatchWebsite/SteamHatchWebsite/App_Start/BundleConfig.cs
+++ SteamHatchWebsite/SteamHatchWebsite/App_Start/BundleConfig.cs
@@ -38,6 +38,13 @@ namespace SteamHatchWebsite
                         "~/Content/themes/base/jquery.ui.datepicker.css",
                         "~/Content/themes/base/jquery.ui.progressbar.css",
                         "~/Content/themes/base/jquery.ui.theme.css"));
+
+            bundles.Add(new StyleBundle("~/Content/BootstrapCss").Include(
+                        "~/Content/bootstrap.css",
+                        "~/Content/bootstrap-responsive.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/BootstrapJs").Include(
+                        "~/Scripts/bootstrap.js"));
         }
     }
 }
