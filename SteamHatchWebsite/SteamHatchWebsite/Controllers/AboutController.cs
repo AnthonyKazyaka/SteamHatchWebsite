1350b60cb342719edf6c1df8c97ee7416b5f87b2
Anthony Kazyaka <ask.anthony@hotmail.com>
2013-08-16 00:33:03 -0400

    Forgot to add unversioned files. Here they are.

---
+++ SteamHatchWebsite/SteamHatchWebsite/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SteamHatchWebsite.Controllers
+{
+    public class AboutController : Controller
+    {
+        //
+        // GET: /About/
+
+        public ActionResult Index()
+        {
+            return View("~/Views/About/About.cshtml");
+        }
+
+    }
+}
